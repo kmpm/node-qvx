b9427700f1ef7650fb020c54ae773e172aea8eff
Peter Magnusson <peter@birchroad.net>
2015-02-24 11:35:52 +0100

    change Outbound._readableState to objectModel=false

--- test/outbound.test.js
+++ test/outbound.test.js
@@ -97,11 +97,51 @@ describe('Outbound', function () {
   });
 
 
-  // lab.test('Transform', function (done) {
-  //   var writer = helper.createWriter();
-  //   var src = require('./fixtures/expressor_single_hash.json');
+  it('aaaahrrdd', function (done) {
 
 
-  // })
+    var fields = {
+      'AddressNumber': {type: DataTypes.FLOAT(8)},
+      'ItemNumber': {type: DataTypes.BIGINT().DECIMALS(0)},
+      'InvoiceDate': {type: DataTypes.TIMESTAMP('utf-8', 1)},
+      'PromisedDeliveryDate': {type: DataTypes.TIMESTAMP}, //'2010-11-19T23:00:00.000Z',
+      'Date': {type: DataTypes.TIMESTAMP}, //'2010-11-19T23:00:00.000Z',
+      'InvoiceNumber': {type: DataTypes.FLOAT(8)},
+      'OrderNumber': {type: DataTypes.FLOAT(8)},
+      'ItemDesc': {type: DataTypes.STRING('utf-8', 4)},
+      'SalesQty': {type: DataTypes.BIGINT().DECIMALS(0)},
+      'OpenQty': {type: DataTypes.BIGINT().DECIMALS(0)},
+      'OpenOrder': {type: DataTypes.BIGINT().DECIMALS(0)},
+      'GrossSales': {type: DataTypes.BIGINT().DECIMALS(0)},
+      'Sales': {type: DataTypes.BIGINT().DECIMALS(0)},
+      'BackOrder': {type: DataTypes.BIGINT().DECIMALS(0)},
+      'Cost': {type: DataTypes.BCD(18).DECIMALS(4)},
+      'Margin': {type: DataTypes.BCD(18).DECIMALS(4)},
+      'SalesKey': {type: DataTypes.STRING('utf-8', 4)},
+      'ofDaysLate': {type: DataTypes.BIGINT().DECIMALS(0)},
+      'ofDaystoShip': {type: DataTypes.BIGINT().DECIMALS(0)}
+    };
+
+    var schema = new qvx.Schema({
+      createdAt: '2012-03-06 19:22:15',
+      creator: false,
+      tableName: 'test',
+      recordFormat: 'object',
+      fields: fields
+    });
+
+    var outbound = new qvx.Outbound(schema);
+
+    var file = fs.createWriteStream(path.join(__dirname, 'tmp', 'streamed.qvx'));
+
+    fs.createReadStream(path.join(__dirname, 'fixtures', 'test_expressor_data_hash.json'))
+    .pipe(es.split())
+    .pipe(es.parse())
+    .pipe(outbound)
+    .pipe(file);
+
+    file.on('close', done);
+
+  });
 
 });//--Writer
